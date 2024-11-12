93c9b306dae66894fe274e2ede17e25895df9a44
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-11 22:39:19 -0500

    Add file upload and info card components

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,10 @@
+import { FileUpload } from '../components';
+
 const Landing: React.FC = () => {
     return (
         <div>
             <h1>Landing</h1>
+            <FileUpload />
         </div>
     );
 };
