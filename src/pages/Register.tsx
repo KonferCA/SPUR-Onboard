d73fc8e2aaddcb5ab1ccc3dcb71986c5f44f55d0
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-17 01:02:40 -0400

    add register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import { RegistrationForm } from "@components";
+
+const Register = () => {
+    return (
+        <>
+            <RegistrationForm />
+        </>
+    );
+};
+
+export { Register };
