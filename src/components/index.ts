34166c399911df061c8bec9e4622e40db46a3d84
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-17 01:02:47 -0400

    add exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { Dropdown } from './Dropdown';
 export { FileUpload } from './FileUpload';
 export { InfoCard } from './InfoCard';
 export { Button } from './Button';
+export { FormContainer } from './FormContainer';
+export { RegistrationForm } from './RegistrationForm';
