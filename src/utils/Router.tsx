fdb9bb4fd1862671e6a5153a8d582d54d93c4028
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-17 01:03:05 -0400

    add register route

--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Landing } from '@pages'
+import { Landing, Register } from '@pages'
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={ <Landing /> } />
+            <Route path="/register" element={ <Register /> } />
         </Routes>
     </BrowserRouter>
 );
