e040dd37c1094e44b602c407bbddb52ad546897a
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-19 15:54:32 -0400

    add textarea export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export { TextInput } from './TextInput';
+export { TextArea } from './TextArea';
 export { Dropdown } from './Dropdown';
 export { FileUpload } from './FileUpload';
 export { InfoCard } from './InfoCard';
 export { Button } from './Button';
 export { FormContainer } from './FormContainer';
-export { RegistrationForm } from './RegistrationForm';
