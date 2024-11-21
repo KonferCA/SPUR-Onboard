1fe7a1979ed1c28a7fddd83cbd11b4aa42ab8040
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-21 17:17:16 -0500

    Update index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Landing } from './Landing';
 export { DashboardPage } from './Dashboard';
 export { AdminDashboardPage } from './AdminDashboard';
+export { SubmitProjectPage } from './SubmitProject';
