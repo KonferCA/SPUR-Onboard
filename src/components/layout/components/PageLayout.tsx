332dc1b6010ddca6c8d0dd8887eb85b7043371f2
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-21 15:52:13 -0500

    Create PageLayout.tsx

---
+++ src/components/layout/components/PageLayout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { Container } from './Container';
+import { Stack } from './Stack';
+
+interface PageLayoutProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export const PageLayout: React.FC<PageLayoutProps> = ({ children, className = '' }) => {
+  return (
+    <Container width="screen" fullHeight className={className}>
+      <Stack gap="none">
+        {children}
+      </Stack>
+    </Container>
+  );
+};
