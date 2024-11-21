adffd5b10e4b22d693be253426f66f94b1118ae3
jc <46619361+juancwu@users.noreply.github.com>
2024-11-21 15:42:46 -0500

    add new @t path alias for internal common types

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@components': path.resolve(__dirname, './src/components'),
-            '@pages': path.resolve(__dirname, './src/pages'),
-            '@utils': path.resolve(__dirname, './src/utils'),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@t': path.resolve(__dirname, './src/types'),
     },
+  },
 });
+
