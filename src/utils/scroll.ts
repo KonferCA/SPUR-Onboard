c2ac580fac89fa2e41c38250d87deeb9193df56a
jc <46619361+juancwu@users.noreply.github.com>
2024-11-22 01:22:18 -0500

    update isElementInView
    only consider not in view when the element is completely not in view

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -24,12 +24,18 @@ export function scrollToWithOffset(target: HTMLElement, offset: number) {
 export function isElementInView(el: Element | null): boolean {
     if (!el) return false;
     const rect = el.getBoundingClientRect();
-    const isInView =
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-            (window.innerWidth || document.documentElement.clientWidth);
-    return isInView;
+
+    const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+    const windowWidth =
+        window.innerWidth || document.documentElement.clientWidth;
+
+    // element is considered in view as long as it's not completely outside the viewport
+    const isNotCompletelyOutOfView =
+        rect.bottom > 0 && // not completely above viewport
+        rect.top < windowHeight && // not completely below viewport
+        rect.right > 0 && // not completely to left of viewport
+        rect.left < windowWidth; // not completely to right of viewport
+
+    return isNotCompletelyOutOfView;
 }
