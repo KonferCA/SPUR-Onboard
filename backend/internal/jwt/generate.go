f2547d39afd5d93afd63ba7292be1de26e19150a
jc <46619361+juancwu@users.noreply.github.com>
2024-12-05 15:40:13 -0500

    add new jwt generate method for verify email token

--- backend/internal/jwt/generate.go
+++ backend/internal/jwt/generate.go
@@ -1,16 +1,20 @@
 package jwt
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"time"
 
 	"KonferCA/SPUR/db"
+
 	golangJWT "github.com/golang-jwt/jwt/v5"
 )
 
 const (
-	ACCESS_TOKEN_TYPE  = "access_token"
-	REFRESH_TOKEN_TYPE = "refresh_token"
+	ACCESS_TOKEN_TYPE       = "access_token"
+	REFRESH_TOKEN_TYPE      = "refresh_token"
+	VERIFY_EMAIL_TOKEN_TYPE = "verify_email_token"
 )
 
 // Generates JWT tokens for the given user. Returns the access token, refresh token and error (nil if no error)
@@ -26,6 +30,45 @@ func Generate(userID string, role db.UserRole) (string, string, error) {
 	}
 
 	return accessToken, refreshToken, nil
+}
+
+/*
+GenerateVerifyEmailToken generates a new token for verifying someones email.
+This method uses a different jwt secret defined by JWT_SECRET_VERIFY_EMAIL
+to separate authentication related jwt with this one.
+*/
+func GenerateVerifyEmailToken(ctx context.Context, email string, id string, exp time.Time) (string, error) {
+	done := make(chan struct {
+		token string
+		err   error
+	})
+
+	go func() {
+		claims := VerifyEmailJWTClaims{
+			Email:     email,
+			TokenType: VERIFY_EMAIL_TOKEN_TYPE,
+			RegisteredClaims: golangJWT.RegisteredClaims{
+				// expire in 1 week
+				ExpiresAt: golangJWT.NewNumericDate(exp),
+				IssuedAt:  golangJWT.NewNumericDate(time.Now()),
+				ID:        id,
+			},
+		}
+
+		token := golangJWT.NewWithClaims(golangJWT.SigningMethodHS256, claims)
+		signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET_VERIFY_EMAIL")))
+		done <- struct {
+			token string
+			err   error
+		}{signedToken, err}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", fmt.Errorf("context canceled while generating verify email JWT: %w", ctx.Err())
+	case result := <-done:
+		return result.token, result.err
+	}
 }
 
 // Private helper method to generate a token.
