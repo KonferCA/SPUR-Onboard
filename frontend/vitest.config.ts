b9fe9a3c77bdd1e1cf272a1b59b0d51fc9f9d114
jc <46619361+juancwu@users.noreply.github.com>
2024-12-06 23:35:04 -0500

    fix test alias

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@/': path.resolve(__dirname, './src'),
+            '@': path.resolve(__dirname, './src'),
             '@assets': path.resolve(__dirname, './src/assets'),
             '@components': path.resolve(__dirname, './src/components'),
             '@components/layout': path.resolve(
