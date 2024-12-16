8d8b89b000ed1afef8ff11785327f9435dab63c7
jc <46619361+juancwu@users.noreply.github.com>
2024-12-16 09:28:17 -0500

    minimal server struct

--- backend/internal/server/index.go
+++ backend/internal/server/index.go
@@ -1,2 +1,46 @@
 package server
 
+import (
+	"KonferCA/SPUR/db"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/labstack/echo/v4"
+)
+
+type Server struct {
+	DBPool *pgxpool.Pool
+	Echo   *echo.Echo
+}
+
+/*
+Initializes a new Server instance and creates a connection pool to the database.
+The database connection string can be controlled by setting the following env variables:
+
+DB_HOST DB_PORT DB_USER DB_PASSWORD DB_NAME DB_SSLMODE
+*/
+func New() (*Server, error) {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+	pool, err := db.NewPool(connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	e := echo.New()
+
+	s := Server{
+		DBPool: pool,
+		Echo:   e,
+	}
+
+	return &s, nil
+}
