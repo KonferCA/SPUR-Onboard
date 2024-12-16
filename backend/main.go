b875e536941135f7995ec3f4defca48de7326db2
jc <46619361+juancwu@users.noreply.github.com>
2024-12-16 11:17:14 -0500

    start server in main

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rs/zerolog/log"
 
 	"KonferCA/SPUR/common"
-	// "KonferCA/SPUR/internal/server"
+	"KonferCA/SPUR/internal/server"
 	"github.com/joho/godotenv"
 )
 
@@ -24,19 +24,18 @@ func main() {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
 	}
 
-	// TODO: replaced with new server
-	// s, err := server.New(false)
-	// if err != nil {
-	// 	log.Fatal().Err(err).Msg("failed to initialized server")
-	// }
-	//
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// }
-	//
-	// err = s.Listen(":" + os.Getenv("PORT"))
-	// if err != nil {
-	// 	log.Fatal().Err(err).Str("PORT", port).Msg("failed to bind server to start accepting connections.")
-	// }
+	s, err := server.New()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialized server")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err = s.Start(port)
+	if err != nil {
+		log.Fatal().Err(err).Str("PORT", port).Msg("failed to bind server to start accepting connections.")
+	}
 }
