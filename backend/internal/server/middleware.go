a3aa32fe6d6668c1437a32cc0f38e2f48572b506
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-12-16 19:09:22 -0500

    Add protected route helper to middleware.go

--- backend/internal/server/middleware.go
+++ backend/internal/server/middleware.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"KonferCA/SPUR/db"
+	"KonferCA/SPUR/internal/jwt"
 	"KonferCA/SPUR/internal/middleware"
+	"github.com/labstack/echo/v4"
 )
 
 /*
@@ -9,4 +12,12 @@ Setup all the global middlewares used in the server.
 */
 func (s *Server) setupMiddlewares() {
 	s.Echo.Use(middleware.RequestID())
+}
+
+// protected is a helper to quickly protect routes with JWT auth
+func (s *Server) protected(roles ...db.UserRole) echo.MiddlewareFunc {
+	return middleware.Auth(middleware.AuthConfig{
+		AcceptTokenType: jwt.ACCESS_TOKEN_TYPE,
+		AcceptUserRoles: roles,
+	}, s.DBPool)
 }
