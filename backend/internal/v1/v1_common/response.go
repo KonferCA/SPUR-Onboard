6762e9e85ca4e6e959a615a3a9ad2c71cc299e3a
jc <46619361+juancwu@users.noreply.github.com>
2024-12-17 16:00:57 -0500

    add two basic response structs for v1

--- backend/internal/v1/v1_common/response.go
+++ backend/internal/v1/v1_common/response.go
@@ -1,1 +1,17 @@
 package v1common
+
+/*
+Use this for any json response that just needs a simple message field.
+*/
+type basicOkResponse struct {
+	Message string `json:"message"`
+}
+
+/*
+Use this for any request that resulted in a failure state.
+This includes any response that was not a 200 or 300 level codes.
+*/
+type basicErrResponse struct {
+	Message   string `json:"message"`
+	RequestId string `json:"request_id"`
+}
