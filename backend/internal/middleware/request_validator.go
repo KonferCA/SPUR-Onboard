335e23ef0f84e25daa0b5e33b0be91b4eed92257
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-12-18 05:24:17 -0400

    add basic request validator + user role validation

---
+++ backend/internal/middleware/request_validator.go
@@ -0,0 +1,59 @@
+package middleware
+
+import (
+	"KonferCA/SPUR/db"
+	"net/http"
+	"reflect"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func NewRequestValidator() *CustomValidator {
+	v := validator.New()
+
+	v.RegisterValidation("valid_user_role", validateUserRole)
+
+	return &CustomValidator{validator: v}
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		log.Error().Err(err).Msg("validation error")
+
+		// TODO: Implement custom error messages
+		return echo.NewHTTPError(http.StatusBadRequest, "err")
+	}
+
+	return nil
+}
+
+func validateUserRole(fl validator.FieldLevel) bool {
+	field := fl.Field()
+
+	if field.Kind() == reflect.String {
+		str := field.String()
+		ur := db.UserRole(str)
+
+		return ur.Valid()
+	}
+
+	if field.Type() == reflect.TypeOf(db.UserRole("")) {
+		ur := field.Interface().(db.UserRole)
+
+		return ur.Valid()
+	}
+
+	if field.Type() == reflect.TypeOf((*db.UserRole)(nil)) && !field.IsNil() {
+		ur := field.Interface().(*db.UserRole)
+
+		return ur.Valid()
+	}
+
+	return false
+}
