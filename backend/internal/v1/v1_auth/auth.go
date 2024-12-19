537011c2414de8b0c1e0cb337ebb2325807b9811
jc <46619361+juancwu@users.noreply.github.com>
2024-12-18 21:26:04 -0500

    update /auth/ami-verified route to use Auth middleware

--- backend/internal/v1/v1_auth/auth.go
+++ backend/internal/v1/v1_auth/auth.go
@@ -2,8 +2,8 @@ package v1_auth
 
 import (
 	"KonferCA/SPUR/db"
-	"KonferCA/SPUR/internal/middleware"
 	"KonferCA/SPUR/internal/v1/v1_common"
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,22 +13,10 @@ import (
 Simple route handler that just returns whether the email has been verified or not in JSON body.
 */
 func (h *Handler) handleEmailVerificationStatus(c echo.Context) error {
-	email := c.QueryParam("email")
-	if email == "" {
-		return v1_common.Fail(c, http.StatusBadRequest, "Missing required query parameter: 'email'", nil)
+	user, ok := c.Get("user").(*db.GetUserByIDRow)
+	if !ok {
+		return v1_common.Fail(c, http.StatusInternalServerError, "", errors.New("Failed to cast user type from context that should have been set by Auth middleware."))
 	}
 
-	ctx := c.Request().Context()
-	logger := middleware.GetLogger(c)
-	pool := h.server.GetDB()
-
-	verified, err := db.New(pool).GetUserEmailVerifiedStatusByEmail(ctx, email)
-	if err != nil {
-		// manually log the error for debugging purposes later
-		// but the client does not need to know any specifics
-		// and instead just know whether it is verified or not.
-		logger.Error(err, "Something went wrong when querying user to check email verification status using email as identifier.")
-	}
-
-	return c.JSON(http.StatusOK, EmailVerifiedStatusResponse{Verified: err == nil && verified})
+	return c.JSON(http.StatusOK, EmailVerifiedStatusResponse{Verified: user.EmailVerified})
 }
--- backend/internal/v1/v1_auth/routes.go
+++ backend/internal/v1/v1_auth/routes.go
@@ -1,12 +1,21 @@
 package v1_auth
 
 import (
+	"KonferCA/SPUR/db"
 	"KonferCA/SPUR/internal/interfaces"
+	"KonferCA/SPUR/internal/middleware"
 
 	"github.com/labstack/echo/v4"
 )
 
+/*
+Sets up the V1 auth routes.
+*/
 func SetupAuthRoutes(e *echo.Group, s interfaces.CoreServer) {
 	h := Handler{server: s}
-	e.GET("/auth/ami-verified", h.handleEmailVerificationStatus)
+	e.GET(
+		"/auth/ami-verified",
+		h.handleEmailVerificationStatus,
+		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin, db.UserRoleStartupOwner),
+	)
 }
