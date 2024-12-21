efe5cfefbafdcc8d452d9ec7e5df491cdd9b3d87
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-12-21 04:26:26 -0400

    add api error types + move basic response here + errortype

--- backend/internal/v1/v1_common/types.go
+++ backend/internal/v1/v1_common/types.go
@@ -1,1 +1,21 @@
 package v1_common
+
+/*
+Use this for any json response that just needs a simple message field.
+*/
+type basicResponse struct {
+	Message string `json:"message"`
+}
+
+type ErrorType string
+
+/*
+Use this for any API response that needs a message and a request_id.
+*/
+type APIError struct {
+	Type      ErrorType `json:"type"`
+	Message   string    `json:"message"`
+	Details   string    `json:"details,omitempty"`
+	RequestID string    `json:"request_id,omitempty`
+	Code      int       `json:"-"`
+}
