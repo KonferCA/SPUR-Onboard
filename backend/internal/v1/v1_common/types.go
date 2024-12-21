c2819bb150272d46d568d133436c1bf0161c23e3
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-12-21 19:48:47 -0400

    update basic response to be global

--- backend/internal/v1/v1_common/types.go
+++ backend/internal/v1/v1_common/types.go
@@ -3,7 +3,7 @@ package v1_common
 /*
 Use this for any json response that just needs a simple message field.
 */
-type basicResponse struct {
+type BasicResponse struct {
 	Message string `json:"message"`
 }
 
@@ -16,6 +16,6 @@ type APIError struct {
 	Type      ErrorType `json:"type"`
 	Message   string    `json:"message"`
 	Details   string    `json:"details,omitempty"`
-	RequestID string    `json:"request_id,omitempty`
+	RequestID string    `json:"request_id,omitempty"`
 	Code      int       `json:"-"`
 }
