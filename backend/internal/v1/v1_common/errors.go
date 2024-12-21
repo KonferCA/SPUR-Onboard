10c8c19a945fae9eebb0833677cbeee24de73049
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-12-21 04:26:11 -0400

    start implementing common error types

--- backend/internal/v1/v1_common/errors.go
+++ backend/internal/v1/v1_common/errors.go
@@ -1,1 +1,35 @@
 package v1_common
+
+/*
+Common error types for v1
+*/
+const (
+	ErrorTypeValidation  ErrorType = "VALIDATION_ERROR"
+	ErrorTypeAuth        ErrorType = "AUTH_ERROR"
+	ErrorTypeNotFound    ErrorType = "NOT_FOUND"
+	ErrorTypeBadRequest  ErrorType = "BAD_REQUEST"
+	ErrorTypeInternal    ErrorType = "INTERNAL_ERROR"
+	ErrorTypeUnavailable ErrorType = "SERVICE_UNAVAILABLE"
+	ErrorTypeForbidden   ErrorType = "FORBIDDEN"
+)
+
+/*
+Use this for any json response that just needs a simple message field.
+*/
+func (e *APIError) Error() string {
+	return e.Message
+}
+
+/*
+NewError creates a new APIError object.
+*/
+func NewError(errorType ErrorType, code int, message string, details string) *APIError {
+	return &APIError{
+		Type:    errorType,
+		Message: message,
+		Details: details,
+		Code:    code,
+	}
+}
+
+// TODO: Implement the rest of the error types
