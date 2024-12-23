76b9657bdb86c6aae7613e3d6edd3f1ebb683a7e
jc <46619361+juancwu@users.noreply.github.com>
2024-12-23 15:50:19 -0500

    increase rate limit in test

--- backend/internal/v1/v1_auth/routes.go
+++ backend/internal/v1/v1_auth/routes.go
@@ -1,9 +1,11 @@
 package v1_auth
 
 import (
+	"KonferCA/SPUR/common"
 	"KonferCA/SPUR/db"
 	"KonferCA/SPUR/internal/interfaces"
 	"KonferCA/SPUR/internal/middleware"
+	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -17,8 +19,13 @@ func SetupAuthRoutes(e *echo.Group, s interfaces.CoreServer) {
 	h := Handler{server: s}
 
 	// 5 request per minute, get block for 15 minutes, and ban up to 1 hour after four blocks.
+	maxRequests := 5
+	if os.Getenv("APP_ENV") == common.TEST_ENV {
+		maxRequests = 5000
+	}
+
 	authLimiter := middleware.NewRateLimiter(&middleware.RateLimiterConfig{
-		Requests:    5,
+		Requests:    maxRequests,
 		Window:      time.Minute,
 		BlockPeriod: time.Minute * 15,
 		MaxBlocks:   4,
