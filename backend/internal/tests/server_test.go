025f99cd72ee78acfce8f21fdc4fe4137574418e
jc <46619361+juancwu@users.noreply.github.com>
2024-12-27 18:44:31 -0500

    update verify email route tests

--- backend/internal/tests/server_test.go
+++ backend/internal/tests/server_test.go
@@ -5,7 +5,6 @@ import (
 	"KonferCA/SPUR/internal/jwt"
 	"KonferCA/SPUR/internal/server"
 	"KonferCA/SPUR/internal/v1/v1_auth"
-	"KonferCA/SPUR/internal/v1/v1_common"
 
 	"bytes"
 	"context"
@@ -18,6 +17,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	golangJWT "github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
@@ -400,30 +400,39 @@ func TestServer(t *testing.T) {
 			s.Echo.ServeHTTP(rec, req)
 			assert.Equal(t, http.StatusOK, rec.Code)
 
-			resBodyBytes, err := io.ReadAll(rec.Body)
-			assert.Nil(t, err)
-
-			var resBody map[string]any
-			err = json.Unmarshal(resBodyBytes, &resBody)
-			assert.Nil(t, err)
-			assert.Equal(t, resBody["verified"], true)
+			doc, err := goquery.NewDocumentFromReader(rec.Body)
+			assert.NoError(t, err)
+			title := doc.Find(`[data-testid="card-title"]`).Text()
+			assert.Equal(t, title, "Email Verified Successfully")
+			details := doc.Find(`[data-testid="card-details"]`).Text()
+			assert.Contains(t, details, "Thank you for verifying your email address")
+			icon := doc.Find(`[data-testid="check-icon"]`)
+			assert.Equal(t, 1, icon.Length())
+			button := doc.Find(`[data-testid="go-to-dashboard"]`)
+			assert.Equal(t, 1, button.Length())
 		})
 
-		t.Run("/auth/verify-email - 400 Bad Request - missing token query parameter", func(t *testing.T) {
+		t.Run("/auth/verify-email - missing token query parameter", func(t *testing.T) {
 			req := httptest.NewRequest(http.MethodGet, "/api/v1/auth/verify-email", nil)
 			rec := httptest.NewRecorder()
 
 			s.Echo.ServeHTTP(rec, req)
 
-			var apiErr v1_common.APIError
-			err := json.NewDecoder(rec.Body).Decode(&apiErr)
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+			doc, err := goquery.NewDocumentFromReader(rec.Body)
 			assert.NoError(t, err)
-			assert.Equal(t, http.StatusBadRequest, rec.Code)
-			assert.Equal(t, v1_common.ErrorTypeBadRequest, apiErr.Type)
-			assert.Equal(t, "Missing required query parameter: 'token'", apiErr.Message)
+			title := doc.Find(`[data-testid="card-title"]`).Text()
+			assert.Equal(t, title, "Failed to Verify Email")
+			details := doc.Find(`[data-testid="card-details"]`).Text()
+			assert.Contains(t, details, "Missing validation token")
+			icon := doc.Find(`[data-testid="x-icon"]`)
+			assert.Equal(t, 1, icon.Length())
+			button := doc.Find(`[data-testid="go-to-dashboard"]`)
+			assert.Equal(t, 1, button.Length())
 		})
 
-		t.Run("/auth/verify-email - 400 Bad Request - deny expired email token", func(t *testing.T) {
+		t.Run("/auth/verify-email - deny expired email token", func(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 			defer cancel()
 			userID, email, _, err := createTestUser(ctx, s)
@@ -440,13 +449,18 @@ func TestServer(t *testing.T) {
 			rec := httptest.NewRecorder()
 
 			s.Echo.ServeHTTP(rec, req)
+			assert.Equal(t, http.StatusOK, rec.Code)
 
-			var apiErr v1_common.APIError
-			err = json.NewDecoder(rec.Body).Decode(&apiErr)
+			doc, err := goquery.NewDocumentFromReader(rec.Body)
 			assert.NoError(t, err)
-			assert.Equal(t, http.StatusBadRequest, rec.Code)
-			assert.Equal(t, v1_common.ErrorTypeBadRequest, apiErr.Type)
-			assert.Equal(t, "Failed to verify email. Invalid or expired token.", apiErr.Message)
+			title := doc.Find(`[data-testid="card-title"]`).Text()
+			assert.Equal(t, title, "Failed to Verify Email")
+			details := doc.Find(`[data-testid="card-details"]`).Text()
+			assert.Contains(t, details, "The verification link is invalid or expired")
+			icon := doc.Find(`[data-testid="x-icon"]`)
+			assert.Equal(t, 1, icon.Length())
+			button := doc.Find(`[data-testid="go-to-dashboard"]`)
+			assert.Equal(t, 1, button.Length())
 		})
 
 		t.Run("/api/v1/auth/logout - 200 OK - successfully logout", func(t *testing.T) {
