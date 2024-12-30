6e6babff5d01bcd8686b25415af9a812cc097c40
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-12-29 20:20:50 -0400

    add company routes

--- backend/internal/v1/setup.go
+++ backend/internal/v1/setup.go
@@ -3,12 +3,15 @@ package v1
 import (
 	"KonferCA/SPUR/internal/interfaces"
 	"KonferCA/SPUR/internal/v1/v1_auth"
+	"KonferCA/SPUR/internal/v1/v1_companies"
 	"KonferCA/SPUR/internal/v1/v1_health"
 )
 
 func SetupRoutes(s interfaces.CoreServer) {
 	e := s.GetEcho()
 	g := e.Group("/api/v1")
+
 	v1_health.SetupHealthcheckRoutes(g, s)
 	v1_auth.SetupAuthRoutes(g, s)
+	v1_companies.SetupCompanyRoutes(g, s)
 }
