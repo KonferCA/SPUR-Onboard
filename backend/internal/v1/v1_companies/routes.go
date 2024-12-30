6f64d50d0806edef57db4b988d491efffbba7ac0
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-12-29 20:30:46 -0400

    add documentation

--- backend/internal/v1/v1_companies/routes.go
+++ backend/internal/v1/v1_companies/routes.go
@@ -19,18 +19,26 @@ func SetupCompanyRoutes(e *echo.Group, s interfaces.CoreServer) {
 	h := NewHandler(s)
 	companies := e.Group("")
 
+	// Setup all the routes for creating a new company
+	// Auth: Startup owners only
 	companies.POST("/company/new", h.handleCreateCompany,
 		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner),
 	)
 
+	// Setup all the routes for getting all companies
+	// Auth: Startup owners and admins
 	companies.GET("/company", h.handleGetCompany,
 		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin),
 	)
 
+	// Setup all the routes for getting a single company
+	// Auth: Admins only
 	companies.GET("/company/:id", h.handleGetCompany,
 		middleware.Auth(s.GetDB(), db.UserRoleAdmin),
 	)
 
+	// Setup all the routes for updating a company
+	// Auth: Startup owners and admins
 	companies.PUT("/company", h.handleUpdateCompany,
 		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin),
 	)
