e6fa0536cac0352f9ba200f3f55d9482214317a2
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2025-01-08 22:42:49 -0500

    Swap roles for perms in jwt types too

--- backend/internal/jwt/types.go
+++ backend/internal/jwt/types.go
@@ -1,14 +1,13 @@
 package jwt
 
 import (
-	"KonferCA/SPUR/db"
 	golangJWT "github.com/golang-jwt/jwt/v5"
 )
 
 type JWTClaims struct {
-	UserID    string      `json:"user_id"`
-	Role      db.UserRole `json:"role"`
-	TokenType string      `json:"token_type"`
+	UserID      string `json:"user_id"`
+	Permissions uint32 `json:"permissions"`
+	TokenType   string `json:"token_type"`
 	golangJWT.RegisteredClaims
 }
 
