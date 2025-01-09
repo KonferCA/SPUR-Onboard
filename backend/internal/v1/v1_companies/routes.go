20bab949e571ec1bffd189d8ed2e1f05760c7f87
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2025-01-08 22:47:30 -0500

    Update routes in v1_companies (forgot)

--- backend/internal/v1/v1_companies/routes.go
+++ backend/internal/v1/v1_companies/routes.go
@@ -1,9 +1,9 @@
 package v1_companies
 
 import (
-	"KonferCA/SPUR/db"
 	"KonferCA/SPUR/internal/interfaces"
 	"KonferCA/SPUR/internal/middleware"
+	"KonferCA/SPUR/internal/permissions"
 
 	"github.com/labstack/echo/v4"
 )
@@ -22,24 +22,24 @@ func SetupCompanyRoutes(e *echo.Group, s interfaces.CoreServer) {
 	// Setup all the routes for creating a new company
 	// Auth: Startup owners only
 	companies.POST("/company/new", h.handleCreateCompany,
-		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner),
+		middleware.Auth(s.GetDB(), permissions.PermSubmitProject),
 	)
 
 	// Setup all the routes for getting all companies
 	// Auth: Startup owners and admins
 	companies.GET("/company", h.handleGetCompany,
-		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin),
+		middleware.Auth(s.GetDB(), permissions.PermSubmitProject, permissions.PermViewAllProjects),
 	)
 
 	// Setup all the routes for getting a single company
 	// Auth: Admins only
 	companies.GET("/company/:id", h.handleGetCompany,
-		middleware.Auth(s.GetDB(), db.UserRoleAdmin),
+		middleware.Auth(s.GetDB(), permissions.PermViewAllProjects),
 	)
 
 	// Setup all the routes for updating a company
 	// Auth: Startup owners and admins
 	companies.PUT("/company", h.handleUpdateCompany,
-		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin),
+		middleware.Auth(s.GetDB(), permissions.PermSubmitProject, permissions.PermManageUsers),
 	)
 }
