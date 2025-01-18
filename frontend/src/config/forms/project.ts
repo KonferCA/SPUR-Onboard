34786b577c7c2dabe00d5f8395b5aeb2846b84ba
jc <46619361+juancwu@users.noreply.github.com>
2025-01-18 02:04:23 -0500

    update form config for project questions
    instead of staticly defined form schema, now it has a helper function
    that groups project questions and sorts them too so that they are in the
    right position to be rendered directly on the page

--- frontend/src/config/forms/project.ts
+++ frontend/src/config/forms/project.ts
@@ -1,217 +1,85 @@
-import type { FormStep } from '@/types';
+import { ProjectQuestion } from '@/services/project';
+import { createZodSchema } from '@/utils/form-validation';
+import { ZodString } from 'zod';
 
-export const projectFormSchema: FormStep[] = [
-    {
-        id: 'A',
-        title: 'Project Information',
-        subtitle: 'Tell us about your project',
-        sections: [
-            {
-                id: 'bookkeeping',
-                title: 'Bookkeeping',
-                fields: [
-                    {
-                        id: 'companyName',
-                        type: 'text',
-                        label: 'What is the name of your company?',
-                        required: true,
-                    },
-                    {
-                        id: 'foundedDate',
-                        type: 'date',
-                        label: 'When was your company founded?',
-                    },
-                    {
-                        id: 'companyStage',
-                        type: 'dropdown',
-                        label: 'What stage is your company at?',
-                        options: [
-                            { id: 1, label: 'Pre-seed', value: 'pre-seed' },
-                            { id: 2, label: 'Seed', value: 'seed' },
-                            { id: 3, label: 'Series A', value: 'series-a' },
-                            { id: 4, label: 'Series B', value: 'series-b' },
-                            {
-                                id: 5,
-                                label: 'Series C+',
-                                value: 'series-c-plus',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'investmentStage',
-                        type: 'dropdown',
-                        label: 'What investment stage is your company at?',
-                        options: [
-                            { id: 1, label: 'Pre-seed', value: 'pre-seed' },
-                            { id: 2, label: 'Seed', value: 'seed' },
-                            { id: 3, label: 'Series A', value: 'series-a' },
-                            { id: 4, label: 'Series B', value: 'series-b' },
-                            {
-                                id: 5,
-                                label: 'Series C+',
-                                value: 'series-c-plus',
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                id: 'company-overview',
-                title: 'Company Overview',
-                description:
-                    'Please do not go into detail about your product in this section, you will have a chance to do so in the following sections of the form.',
-                fields: [
-                    {
-                        id: 'description',
-                        type: 'textarea',
-                        label: 'Brief description of your company',
-                        required: true,
-                        rows: 4,
-                    },
-                    {
-                        id: 'inspiration',
-                        type: 'textarea',
-                        label: "What inspired you to start this company, and what is the core problem you're solving?",
-                        required: true,
-                        rows: 4,
-                    },
-                    {
-                        id: 'vision',
-                        type: 'textarea',
-                        label: 'What is your long-term vision for the company, and how do you plan to disrupt or lead your market?',
-                        required: true,
-                        rows: 4,
-                    },
-                ],
-            },
-            {
-                id: 'product-overview',
-                title: 'Product Overview',
-                description: '',
-                fields: [
-                    {
-                        id: 'product-description',
-                        type: 'textarea',
-                        label: 'How does your product or idea differentiate from competitors, and what makes it defensible or unique?',
-                        required: true,
-                    },
-                    {
-                        id: 'product-roadmap',
-                        type: 'textarea',
-                        label: 'What does your current product roadmap look like?',
-                        required: true,
-                    },
-                ],
-            },
-            {
-                id: 'customer-demographic',
-                title: 'Customer & Demographic',
-                description: '',
-                fields: [
-                    {
-                        id: 'target-demographic',
-                        type: 'textarea',
-                        label: 'Who are your target demographic and customers? What makes your product or service compelling to them?',
-                        required: true,
-                    },
-                    {
-                        id: 'addressable-market',
-                        type: 'textarea',
-                        label: 'What is the total addressable market for this company? How do you plan to capture your share of it?',
-                        required: true,
-                    },
-                ],
-            },
-            {
-                id: 'financials',
-                title: 'Financials',
-                description: '',
-                fields: [
-                    {
-                        id: 'company-revenue',
-                        type: 'text',
-                        label: "What is your company's annual recurring revenue (ARR)?",
-                        required: true,
-                    },
-                    {
-                        id: 'company-raised',
-                        type: 'text',
-                        label: 'How much has your company raised so far?',
-                        required: true,
-                    },
-                    {
-                        id: 'company-valuation',
-                        type: 'text',
-                        label: "What is your company's current valuation?",
-                        required: true,
-                    },
-                    {
-                        id: 'company-monthly-expenses',
-                        type: 'text',
-                        label: "What are your company's monthly expenses?",
-                        required: true,
-                    },
-                    {
-                        id: 'company-main-revenue',
-                        type: 'textarea',
-                        label: 'What are your main revenue streams, and how do you project these will grow over the next 12-36 months?',
-                        required: true,
-                    },
-                    {
-                        id: 'company-funding',
-                        type: 'textarea',
-                        label: 'What funding have you raised so far, and how have you allocated that capital towards growth and development?',
-                        required: true,
-                    },
-                ],
-            },
-            {
-                id: 'team-overview',
-                title: 'Team Overview',
-                description: 'Tell us about your team members',
-                fields: [
-                    {
-                        id: 'team-members',
-                        type: 'team-members',
-                        label: 'Team Members',
-                        required: true,
-                    },
-                ],
-            },
-            {
-                id: 'social-media',
-                title: 'Social Media & Web Presence',
-                description:
-                    "Please share with us your online presence so we can get a better understanding of your organization's identity. You can share pages like your website, LinkedIn, Instagram etc...",
-                fields: [
-                    {
-                        id: 'social-links',
-                        type: 'social-links',
-                        label: 'Social Links',
-                        required: true,
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        id: 'B',
-        title: 'Part B',
-        subtitle: 'Document uploads',
-        sections: [
-            {
-                id: 'document-upload',
-                title: 'Upload company business documents',
-                description:
-                    "To help us verify your organization's legitimacy, please provide us with business related documents.",
-                fields: [
-                    {
-                        id: 'documents',
-                        type: 'file',
-                        label: 'Upload your documents',
-                    },
-                ],
-            },
-        ],
-    },
+export const sectionsOrder: string[] = [
+    'the basics',
+    'the team',
+    'the financials',
 ];
+
+export interface GroupedProjectQuestions {
+    // section basically serves as the id of the group
+    section: string;
+    subSections: SubSection[];
+}
+
+export interface SubSection {
+    id: string;
+    question: string;
+    section: string;
+    subSection: string;
+    required: boolean;
+    validations: ZodString[];
+    options?: string[];
+}
+
+/*
+ * This will sort based on the sections order array and group the questions that belong
+ * to the same section together. The questions in a section will also be sorted based on their
+ * sub section order.
+ */
+export function groupProjectQuestions(
+    questions: ProjectQuestion[]
+): GroupedProjectQuestions[] {
+    // Sort questions based on all order fields
+    const sortedQuestions = [...questions].sort((a, b) => {
+        // First sort by section_order
+        if (a.sectionOrder !== b.sectionOrder) {
+            return a.sectionOrder - b.sectionOrder;
+        }
+
+        // If same section, sort by sub_section_order
+        if (a.subSectionOrder !== b.subSectionOrder) {
+            return a.subSectionOrder - b.subSectionOrder;
+        }
+
+        // If same subsection, sort by question_order
+        return a.questionOrder - b.questionOrder;
+    });
+
+    // Group questions by section
+    const groupedBySection = sortedQuestions.reduce<GroupedProjectQuestions[]>(
+        (acc, question) => {
+            // Find existing group for this section
+            let group = acc.find((g) => g.section === question.section);
+
+            // Create SubSection object from the question
+            const subSection: SubSection = {
+                id: question.id,
+                question: question.question,
+                section: question.section,
+                subSection: question.subSection,
+                required: question.required,
+                validations: createZodSchema(question.validations),
+                options: question.options,
+            };
+
+            if (group) {
+                // Add to existing group
+                group.subSections.push(subSection);
+            } else {
+                // Create new group
+                acc.push({
+                    section: question.section,
+                    subSections: [subSection],
+                });
+            }
+
+            return acc;
+        },
+        []
+    );
+
+    return groupedBySection;
+}
