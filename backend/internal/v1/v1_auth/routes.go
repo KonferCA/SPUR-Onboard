b618aeb29d6eb024939ab9e6e51a2c261f01e6e7
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-01-28 20:58:54 -0400

    readd rate limiting

--- backend/internal/v1/v1_auth/routes.go
+++ backend/internal/v1/v1_auth/routes.go
@@ -1,9 +1,12 @@
 package v1_auth
 
 import (
+	"KonferCA/SPUR/common"
 	"KonferCA/SPUR/db"
 	"KonferCA/SPUR/internal/interfaces"
 	"KonferCA/SPUR/internal/middleware"
+	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -16,28 +19,17 @@ func SetupAuthRoutes(e *echo.Group, s interfaces.CoreServer) {
 	h := Handler{server: s}
 
 	// 5 request per minute, get block for 15 minutes, and ban up to 1 hour after four blocks.
-	// maxRequests := 5
-	// if os.Getenv("APP_ENV") == common.TEST_ENV {
-	// 	maxRequests = 5000
-	// }
+	maxRequests := 5
+	if os.Getenv("APP_ENV") == common.TEST_ENV {
+		maxRequests = 5000
+	}
 
-	// authLimiter := middleware.NewRateLimiter(&middleware.RateLimiterConfig{
-	// 	Requests:    maxRequests,
-	// 	Window:      time.Minute,
-	// 	BlockPeriod: time.Minute * 15,
-	// 	MaxBlocks:   4,
-	// })
-
-	// e.POST("/auth/login", h.handleLogin)
-	// e.GET(
-	// 	"/auth/ami-verified",
-	// 	h.handleEmailVerificationStatus,
-	// 	middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin),
-	// )
-	// e.GET("/auth/verify", h.handleVerifyCookie)
-	// e.GET("/auth/verify-email", h.handleVerifyEmail, authLimiter.RateLimit())
-	// e.POST("/auth/register", h.handleRegister, authLimiter.RateLimit())
-	// e.POST("/auth/logout", h.handleLogout, authLimiter.RateLimit())
+	authLimiter := middleware.NewRateLimiter(&middleware.RateLimiterConfig{
+		Requests:    maxRequests,
+		Window:      time.Minute,
+		BlockPeriod: time.Minute * 15,
+		MaxBlocks:   4,
+	})
 
 	e.POST("/auth/login", h.handleLogin)
 	e.GET(
@@ -46,7 +38,7 @@ func SetupAuthRoutes(e *echo.Group, s interfaces.CoreServer) {
 		middleware.Auth(s.GetDB(), db.UserRoleStartupOwner, db.UserRoleAdmin),
 	)
 	e.GET("/auth/verify", h.handleVerifyCookie)
-	e.GET("/auth/verify-email", h.handleVerifyEmail)
-	e.POST("/auth/register", h.handleRegister)
-	e.POST("/auth/logout", h.handleLogout)
+	e.GET("/auth/verify-email", h.handleVerifyEmail, authLimiter.RateLimit())
+	e.POST("/auth/register", h.handleRegister, authLimiter.RateLimit())
+	e.POST("/auth/logout", h.handleLogout, authLimiter.RateLimit())
 }
