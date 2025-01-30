e263cb74d357b0739a3817880c503ad0b5cb5a3b
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-01-30 19:31:35 -0400

    update exports

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,6 +1,35 @@
-export { register, signin, signout } from './auth';
-export { RegisterError, ApiError, API_ERROR, REGISTER_ERROR } from './errors';
-export { createProject } from './project';
-export { createCompany, getCompany } from './company';
-export { uploadFile } from './storage';
-export { checkVerificationStatus,  handleEmailVerificationRedirect } from './verification';
+export {
+    register,
+    signin,
+    signout,
+    refreshAccessToken,
+    type AuthResponse,
+    type SigninResponse,
+    type RegisterReponse
+} from './auth';
+
+export {
+    RegisterError,
+    ApiError,
+    API_ERROR,
+    REGISTER_ERROR
+} from './errors';
+
+export {
+    createProject
+} from './project';
+
+export {
+    createCompany,
+    getCompany
+} from './company';
+
+export {
+    uploadFile
+} from './storage';
+
+export {
+    checkVerificationStatus,
+    handleEmailVerificationRedirect,
+    isVerificationRedirect
+} from './verification';
