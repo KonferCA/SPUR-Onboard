5b510e385897ceca82f5219be69abb244887dbd2
jc <46619361+juancwu@users.noreply.github.com>
2025-02-04 19:21:21 -0500

    increase clickable area and add hover effect for "back to dashboard" link in project form pages

--- frontend/src/pages/user/_auth/project/$projectId.form.tsx
+++ frontend/src/pages/user/_auth/project/$projectId.form.tsx
@@ -514,7 +514,10 @@ function ProjectFormPage() {
             <nav className="fixed top-0 left-0 right-0 z-50 bg-white h-24 border-b border-gray-300">
                 <ul className="flex items-center pl-4 h-full">
                     <li>
-                        <Link to="/user/dashboard">
+                        <Link
+                            to="/user/dashboard"
+                            className="transition p-2 inline-block rounded-lg hover:bg-gray-100"
+                        >
                             <div className="flex items-center gap-2">
                                 <span>
                                     <IoMdArrowRoundBack />
