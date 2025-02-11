af5d2fd43116c17dc54b11f54e232678723371ea
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-02-10 22:18:24 -0500

    reformat env naming + add documentation

--- backend/common/env_name.go
+++ backend/common/env_name.go
@@ -1,7 +1,16 @@
 package common
 
-const PRODUCTION_ENV = "production"
-const DEVELOPMENT_ENV = "development"
-const PREVIEW_ENV = "preview"
-const TEST_ENV = "test"
-const STAGING_ENV = "staging"
+/*
+ * The different environments the application can run in.
+ */
+const (
+	PRODUCTION_ENV = "production"
+
+	DEVELOPMENT_ENV = "development"
+
+	PREVIEW_ENV = "preview"
+
+	TEST_ENV = "test"
+
+	STAGING_ENV = "staging"
+)
