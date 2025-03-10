a738f89a24a67ba227c9cc30a06d3e8712bf80ee
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2025-03-10 19:33:31 -0400

    Update index.ts

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -2,13 +2,3 @@ export * from './common';
 export * from './form';
 export type { User, UserRole } from './auth';
 export * from './company';
-
-export type FormFieldType =
-    | 'textinput'
-    | 'textarea'
-    | 'select'
-    | 'multiselect'
-    | 'file'
-    | 'team'
-    | 'date'
-    | 'fundingstructure';
